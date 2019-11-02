df365bc71c5e7d54c02525478a80456b68fbe55a
James Luong <jamesluong@hotmail.co.uk>
2019-11-02 15:55:47 +0000

    website

--- website/web.py
+++ website/web.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route('/about')
 def about():
-   return render_template("about.html")
+   return render_template("about.html", title = "about")
 
 if __name__ == '__main__':
     app.run(debug=True)
