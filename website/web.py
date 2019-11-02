5a24423c5d2e0e611a623e9bdae2d8861fab5cf9
James Luong <jamesluong@hotmail.co.uk>
2019-11-02 15:31:40 +0000

    website

--- website/web.py
+++ website/web.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     return render_template("home.html")
    
 
 @app.route('/about')
 def about():
-   return "<h1>About</h1>"
+   return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
